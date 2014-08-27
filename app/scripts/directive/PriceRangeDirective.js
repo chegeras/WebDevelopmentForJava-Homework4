096b15d2c9d90f6529172cfeb95dd7151e0389e8
Nikolay <chegeras@gmail.com>
2014-08-27 19:16:38 +0400

    slider binding

--- app/scripts/directive/PriceRangeDirective.js
+++ app/scripts/directive/PriceRangeDirective.js
@@ -1,7 +1,8 @@
 (function () {
   'use strict';
 
-  var priceRangeDirectiveFactory = function () {
+  var priceRangeDirectiveFactory = function (productService) {
+
     return {
       scope: {
         minPrice : '@',
@@ -12,12 +13,14 @@
       restrict: 'E',
       templateUrl: 'views/partial/PriceRangeDirective.html',
       link: function (scope, element) {
+
+          var storage = angular.module('auction').service('ProductService');
         var slider = angular.element(element).find('input[type=text]'),
             min = scope.minPrice || 0,
             max = scope.maxPrice || 500;
 
-        scope.lowPrice  = scope.lowPrice  || min;
-        scope.highPrice = scope.highPrice || max;
+        scope.lowPrice  = storage.lowPrice ||scope.lowPrice  || min;
+        scope.highPrice = storage.highPrice||scope.highPrice || max;
 
         // Initialize slider
         slider.slider({
@@ -28,12 +31,20 @@
             scope.highPrice
           ]
         });
-
         // Slider -> numeric fields
         slider.on('slideStop', function (event) {
           scope.$apply(function () {
-            if (scope.lowPrice  !== event.value[0]) { scope.lowPrice  = event.value[0]; }
-            if (scope.highPrice !== event.value[1]) { scope.highPrice = event.value[1]; }
+            if (scope.lowPrice  !== event.value[0])
+            {
+                scope.lowPrice  = event.value[0];
+                storage.lowPrice = scope.lowPrice;
+            }
+
+            if (scope.highPrice !== event.value[1])
+            {
+                scope.highPrice = event.value[1];
+                storage.highPrice = scope.highPrice;
+            }
           });
         });
 
@@ -41,11 +52,12 @@
         var currentVal = function () { return slider.slider('getValue'); };
         var setSlider = function (low, high) { slider.slider('setValue', [low, high]); };
 
-        scope.$watch('lowPrice',  function (newVal) { setSlider(newVal || min, currentVal()[1]); });
-        scope.$watch('highPrice', function (newVal) { setSlider(currentVal()[0], newVal || max); });
+        scope.$watch('lowPrice',  function (newVal) { setSlider(newVal ||storage.lowPrice|| min, currentVal()[1]) });
+        scope.$watch('highPrice', function (newVal) { setSlider(currentVal()[0], newVal ||storage.highPrice|| max); });
       }
     };
   };
 
+  priceRangeDirectiveFactory.$inject = ['ProductService'];
   angular.module('auction').directive('auctionPriceRange', priceRangeDirectiveFactory);
 }());
